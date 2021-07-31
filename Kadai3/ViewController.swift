6cabfea4084d70ff6c934316ba9d55721ecf05e6
Rin <rin.t.4921@gmail.com>
2021-07-31 20:10:03 +0900

    first commit

---
+++ Kadai3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kadai3
+//
+//  Created by Rin on 2021/07/31.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
