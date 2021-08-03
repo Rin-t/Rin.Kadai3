4b0da6c9545b2ffef8e9d9e1570c7350e7d4f5dd
akio0911 <akio0911@gmail.com>
2021-08-04 06:51:10 +0900

    feedback

--- Kadai3/ViewController.swift
+++ Kadai3/ViewController.swift
@@ -16,13 +16,26 @@ final class ViewController: UIViewController {
     @IBOutlet private weak var num2Label: UILabel!
     @IBOutlet private weak var resultCalculationLabel: UILabel!
 
+    struct ViewGroup {
+        var textField: UITextField
+        var `switch`: UISwitch
+        var label: UILabel
+    }
+
+    var viewGroups: [ViewGroup] {
+        [
+            ViewGroup(textField: textField1, switch: switch1, label: num1Label),
+            ViewGroup(textField: textField2, switch: switch2, label: num2Label)
+        ]
+    }
+
     @IBAction private func didTapButton(_ sender: UIButton) {
-        let num1 = Double(textField1.text ?? "") ?? 0
-        let num2 = Double(textField2.text ?? "") ?? 0
-        let matchedSignNum1 = switch1.isOn ? -num1 : num1
-        let matchedSignNum2 = switch2.isOn ? -num2 : num2
-        num1Label.text = String(matchedSignNum1)
-        num2Label.text = String(matchedSignNum2)
-        resultCalculationLabel.text = String(matchedSignNum1 + matchedSignNum2)
+        let signedNumbers = viewGroups
+            .map { (Double($0.textField.text ?? "") ?? 0) * ($0.switch.isOn ? -1 : 1) }
+
+        zip(viewGroups, signedNumbers)
+            .forEach { $0.0.label.text = String($0.1) }
+
+        resultCalculationLabel.text = String(signedNumbers.reduce(0, +))
     }
 }
